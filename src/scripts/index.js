80c50e0017022526feffc1c02a21758819f2ea78
patrickthumeyer <p.thumeyer@gmail.com>
2020-02-27 14:09:25 +0100

    first commit

---
+++ src/scripts/index.js
@@ -0,0 +1,6 @@
+// The following line makes sure your styles are included in the project. Don't remove this.
+import "../styles/main.scss";
+import "regenerator-runtime/runtime";
+// Import any additional modules you want to include below \/
+
+// \/ All of your javascript should go here \/
